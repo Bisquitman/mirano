c8d9f92b1bc382f82891f261712d7eec3fdae324
Bisquitman <lynx8309@gmail.com>
2024-05-03 11:04:45 +0300

    вёрстка, адаптив, фильтр

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
 import {defineConfig} from "vite";
+import Legacy from "@vitejs/plugin-legacy";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
+  plugins: [
+    Legacy({
+      targets: ["defaults", "IE >= 8", "Chrome >= 49"],
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {},
+    },
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+  },
   resolve: {
     alias: {
       "@": "/src",
