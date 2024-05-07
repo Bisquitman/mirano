b0e5ab987c27d60a5ce9f2ab3b5eead701ef8a66
Bisquitman <lynx8309@gmail.com>
2024-05-07 08:42:39 +0300

    Добавил Vite plugin image optimizer

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,27 @@
 import {defineConfig} from "vite";
 import Legacy from "@vitejs/plugin-legacy";
 import autoprefixer from "autoprefixer";
+import {ViteImageOptimizer} from "vite-plugin-image-optimizer";
 
 export default defineConfig({
   plugins: [
+    ViteImageOptimizer({
+      jpg: {
+        quality: 85,
+      },
+      jpeg: {
+        quality: 85,
+      },
+      png: {
+        quality: 80,
+      },
+      webp: {
+        quality: 80,
+      },
+      avif: {
+        quality: 60,
+      },
+    }),
     Legacy({
       targets: ["defaults", "IE >= 8", "Chrome >= 49"],
     }),
