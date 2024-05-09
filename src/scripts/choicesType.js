185a3dced8fbb74cc5145d93c6b15d10b8f67bb1
Bisquitman <lynx8309@gmail.com>
2024-05-09 20:57:46 +0300

    Обновление категорий из Store

--- src/scripts/choicesType.js
+++ src/scripts/choicesType.js
@@ -1,15 +1,25 @@
 import {store} from "@/scripts/Store";
+import {CategoryItem} from "@/scripts/CategoryItem";
 
 export const initChoicesType = () => {
   const typeChoices = document.querySelector('.filter__choices_type');
+  const choicesList = document.querySelector('.filter__type-list');
 
   const updateTypeChoicesVisibility = () => {
-    const category = store.getCategories();
-    console.log(category);
+    const categories = store.getCategories();
+    // console.log("categories", categories);
 
-    if (category.size) {
+    if (categories.size) {
       typeChoices.style.display = '';
     //   !todo обновить категории
+    //   -----------------------------------
+      choicesList.innerHTML = '';
+      categories.forEach((category) => {
+        console.log(category);
+        const categoryItem = CategoryItem(category);
+        choicesList.append(categoryItem);
+      });
+    //   ------------------------------------
     } else {
       typeChoices.style.display = 'none';
     }
